b7d43448b284831591276c2d14e8dc8a49c7fb24
ChrossEM <cemc_cris@hotmail.com>
2023-10-11 15:40:52 -0600

    added mains

--- src/main/java/org/example/HibernateAsociacionesManyToManyFindBidireccional.java
+++ src/main/java/org/example/HibernateAsociacionesManyToManyFindBidireccional.java
@@ -5,26 +5,24 @@ import org.example.entity.Alumno;
 import org.example.entity.Curso;
 import org.example.util.JpaUtil;
 
-import java.util.Currency;
-
-public class HibernateAsociacionesManyToManyBidireccional {
+public class HibernateAsociacionesManyToManyFindBidireccional {
     public static void main(String[] args) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try{
-            em.getTransaction().begin();
-            Alumno alumno1 = new Alumno("Maria", "Rihna");
-            Alumno alumno2 = new Alumno("Guillemrmo", "Staruke");
 
-            Curso curso1 = new Curso("Curso Java", "Andres");
-            Curso curso2 = new Curso("Curso Hibernate y Jpa", "Andres");
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+            Alumno alumno1 = em.find(Alumno.class, 1L);
+            Alumno alumno2 = em.find(Alumno.class, 2L);
+
+            Curso curso1 = em.find(Curso.class, 1L);// new Curso("Curso Java", "Andres");
+            Curso curso2 = em.find(Curso.class, 2L);// new Curso("Curso Hibernate y Jpa", "Andres");
 
             alumno1.addCurso(curso1);
             alumno1.addCurso(curso2);
 
             alumno2.addCurso(curso1);
-
-            em.persist(alumno1);
-            em.persist(alumno2);
 
             em.getTransaction().commit();
 
@@ -32,12 +30,13 @@ public class HibernateAsociacionesManyToManyBidireccional {
             System.out.println(alumno2);
 
             em.getTransaction().begin();
-            Curso c2 =new Curso("Curso Java","Andres");
-            c2.setId(4L);
+            // Curso c2 = em.find(Curso.class, 3L);
+            Curso c2 = new Curso("Curso Java EE 9", "Andres");
+            c2.setId(2L);
             alumno1.removeCurso(c2);
             em.getTransaction().commit();
             System.out.println(alumno1);
-        } catch (Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
             e.printStackTrace();
         } finally {
